3a6fb7874737bc1d8907b182a597a5065c3acdea
Richard Kuo <richard.p.kuo@gmail.com>
2018-02-18 01:43:28 -0700

    hacking

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,18 +3,33 @@ class Product < ApplicationRecord
 
   after_create :generate_keys
 
+  possible_public_chars = ['0','1','2','3','4','5','6','7','8','9','a','c','d','e','f']
+
   def sell(wallet_key)
     self.wallet_key = wallet_key
     save!
   end
 
   def generate_keys
-    self.public_key = Digest::SHA2.hexdigest(self.public_id)
+    public_eth_key = '0x'
+
+    40.times do |number|
+      public_eth_key = public_eth_key + possible_public_chars[Random.rand(possible_public_chars.count - 1)]
+    end
+
+    private_eth_key = ''
+
+    64.times do |number|
+      private_eth_key = private_eth_key + possible_public_chars[Random.rand(possible_public_chars.count - 1)]
+    end
+
+    self.public_key = public_eth_key
+    self.private_key = private_eth_key
     self.save!
   end
 
   def generate_ethereum_keys
-    self.public_key = response.public_key
+    # self.public_key = response.public_key
     self.save!
   end
 
